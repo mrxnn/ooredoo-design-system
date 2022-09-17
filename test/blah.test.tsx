abee1f08e0f637e222336df16da907ea1c066e86
Mayura <mayura.ramanayake@gmail.com>
2022-09-18 03:34:43 +0530

    Update the tests

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Filled } from '../stories/Button.stories';
 
-describe('Thing', () => {
+describe('Filled', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Filled />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
