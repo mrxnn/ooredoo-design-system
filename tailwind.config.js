af304c8045d0cca84768ba48c3a8647eddb4d08c
Mayura <mayura.ramanayake@gmail.com>
2022-09-19 09:53:43 +0530

    Add Rubik typeface

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Rubik', ...fontFamily.sans],
+      },
       colors: {
         primary: {
           red: '#FF0000',
