49cb9abfc63a81a7f81c5f2728dca1c8b3bf4b24
Mayura <mayura.ramanayake@gmail.com>
2022-09-22 08:04:18 +0530

    Add ooredoo cards

--- stories/UsageCard.stories.tsx
+++ stories/UsageCard.stories.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { UsageCard, Props } from '../src/components/UsageCard';
-import sim_card_img from '../static/icons/sim_card.png';
-import box_img from '../static/icons/box_perspective.png';
+
+const sim_card = require('../static/icons/sim_card.png');
+// import sim_card_img from '../static/icons/sim_card.png';
 
 const meta: Meta = {
   title: 'components/UsageCard',
@@ -16,14 +17,14 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story<Props> = args => (
+const Template: Story<Props> = (args) => (
   <UsageCard {...args} className="w-[360px]" />
 );
 
 export const Default = Template.bind({});
 
 Default.args = {
-  icon: <img src={sim_card_img} alt="Sim Card" />,
+  icon: <img src={sim_card} alt="Sim Card" />,
   type: 'Purchases',
   title: 'Maxi Internet 1000',
   description: 'Free 020 + 15min Offnet',
